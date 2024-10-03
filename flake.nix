a893ec1f808ac87b5547bbb31471ebb8f74bf0aa
notimadam123 <171095855+notimadam123@users.noreply.github.com>
2024-10-03 23:43:51 +0100

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  description = "hyprland nix config"
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixcord = {
+      url = "github:kaylorben/nixcord";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    
+    ags.url = "github:Aylur.ags";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+
+    systems = [
+    "x86_64-linux"
+    ];
+  in {
+    nixosConfigurations = {
+      tjd = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        modules = [
+          ./hosts/tjd/configuration.nix
+        ];
+      };
+    };
+   }
+  }
+};
