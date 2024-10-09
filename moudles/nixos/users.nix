633801d35e2f829085a3cf47859efc3c8716c22b
notimadam123 <171095855+notimadam123@users.noreply.github.com>
2024-10-09 18:45:00 +0100

    f

--- moudles/nixos/users.nix
+++ moudles/nixos/users.nix
@@ -1,10 +1,23 @@
-{ config, pkgs, ... }: {
-  users = {
-    defaultUserShell = pkgs.fish;
-    users.${config.var.username} = {
-      isNormaluser = true;
-      description = "${config.var.username} account";
-      extraGroups = [ "networkmanager" "wheel" "plugdev" "libvirt" ];
+{ pkgs, inputs, username, host, ...}:
+{
+  imports = [ inputs.home-manager.nixosModules.home-manager ];
+  home-manager = {
+    useUserPackages = true;
+    useGlobalPkgs = true;
+    extraSpecialArgs = { inherit inputs username host; };
+    users.${username} = {
+      home.username = "${username}";
+      home.homeDirectory = "/home/${username}";
+      home.stateVersion = "24.11";
+      programs.home-manager.enable = true;
     };
   };
+
+  users.users.${username} = {
+    isNormalUser = true;
+    description = "${username}";
+    extraGroups = [ "libvirt" "networkmanager" "wheel" "plugdev" ];
+    shell = pkgs.fish;
+  };
+  nix.settings.allowed-users = [ "${username}" ];
 }
