9a9d8d27f4166fc663494077beee1af594d57fc5
notimadam123 <171095855+notimadam123@users.noreply.github.com>
2024-10-09 21:17:50 +0100

    f

--- modules/nixos/users.nix
+++ modules/nixos/users.nix
@@ -1,13 +1,14 @@
-{ pkgs, inputs, username, host, ...}:
-{
+{ config, pkgs, inputs, username, host, ...}:
+let username = config.var.username;
+in {
   imports = [ inputs.home-manager.nixosModules.home-manager ];
   home-manager = {
     useUserPackages = true;
     useGlobalPkgs = true;
     extraSpecialArgs = { inherit inputs username host; };
     users.${username} = {
-      home.username = "${config.var.username}";
-      home.homeDirectory = "/home/${config.var.username}";
+      home.username = "${username}";
+      home.homeDirectory = "/home/${username}";
       home.stateVersion = "24.11";
       programs.home-manager.enable = true;
     };
@@ -19,5 +20,5 @@
     extraGroups = [ "libvirt" "networkmanager" "wheel" "plugdev" ];
     shell = pkgs.fish;
   };
-  nix.settings.allowed-users = [ "${config.var.username}" ];
+  nix.settings.allowed-users = [ "${username}" ];
 }
