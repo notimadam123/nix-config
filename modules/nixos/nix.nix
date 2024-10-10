0a8e7d4bcaf3a06773f657e73f6d657626e0d94d
notimadam123 <171095855+notimadam123@users.noreply.github.com>
2024-10-10 18:46:01 +0100

    fix

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -12,11 +12,5 @@
        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
      ];
    };
-   gc = {
-     automatic = config.var.autoGarbageCollector;
-     persistent = true;
-     dates = "weekly";
-     options = "--delete-older-than 7d";
-   };
  };
 }
